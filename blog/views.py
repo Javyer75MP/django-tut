da344907bc23d3078f46a6ecd706ded0ca05e483
Francisco Javier Montiel <javyer75@gmail.com>
2016-04-11 12:42:47 +0200

    django-tut_v3

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post
-from .forms import  PostForm
+from .models import Post, Comment
+from .forms import  PostForm, CommentForm
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,7 +11,21 @@ def post_list(request):
 def post_detail(request, pk):
 
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post' : post})
+    comments = Comment.objects.filter(post=post).order_by('created_date')
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author =request.user
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/post_detail.html', {'post' : post, 'comments' : comments, 'form' : form})
 
 def post_new(request):
     if request.method == "POST":
@@ -27,6 +42,7 @@ def post_new(request):
 
 def post_edit(request, pk):
         post = get_object_or_404(Post, pk=pk)
+
         if request.method == "POST":
             form = PostForm(request.POST, instance=post)
             if form.is_valid():
